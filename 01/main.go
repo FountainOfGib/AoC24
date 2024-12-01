e15559c6dbf320f798028fd40e3ac06559948db9
FountainOfGib <dfo4242@gmail.com>
2024-12-01 16:31:27 +0000

    1 star!

--- 01/main.go
+++ 01/main.go
@@ -7,7 +7,18 @@ import (
 	"fmt"
 	"os"
 	"sort"
+	"strconv"
+	"strings"
 )
+
+func failHardParseInt(number string) int {
+	val, err := strconv.Atoi(number)
+	if err != nil {
+		fmt.Println("err file contains none ints")
+		os.Exit(1) // I don't care that this is bad. Go cry.
+	}
+	return val
+}
 
 func parseInput(fileName string) ([]int, []int) {
 	A, B := []int{}, []int{}
@@ -15,16 +26,21 @@ func parseInput(fileName string) ([]int, []int) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println("err opening", fileName)
-		os.Exit(1)
-		// return {}, {}, err
+		os.Exit(1) // I don't care that this is bad. Go cry.
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+		line := strings.Split(scanner.Text(), "   ")
+		if len(line) != 2 {
+			fmt.Println("err in", fileName, ". must be 2 values separated by 3 spaces")
+			os.Exit(1) // I don't care that this is bad. Go cry.
+		}
+		A = append(A, failHardParseInt(line[0]))
+		B = append(B, failHardParseInt(line[1]))
+		fmt.Print("\n")
 	}
 	return A, B
 }
@@ -34,11 +50,16 @@ func main() {
 	diff := []int{}
 	sort.Ints(A)
 	sort.Ints(B)
-	for i := 0; i < len(A); i++ {
+	for i, _ := range A {
 		delta := A[i] - B[i]
 		diff = append(diff, max(delta, delta*-1))
 	}
 	fmt.Println("_./*\\._./*\\._./*\\._./*\\._./*\\._")
 	fmt.Println(diff)
 	fmt.Println("*\\._./*\\._./*\\._./*\\._./*\\._./")
+	sum := 0
+	for _, num := range diff {
+		sum += num
+	}
+	fmt.Println(sum)
 }
