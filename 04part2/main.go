2af1e55f974998521c85222b20e4f406a760518a
FountainOfGib <dfo4242@gmail.com>
2024-12-05 00:10:05 +0000

    8 stars

--- 04part2/main.go
+++ 04part2/main.go
@@ -33,91 +33,25 @@ func fileToSliceOfStrings(filePath string) ([]string, error) {
 	return lines, nil
 }
 
-func getMatchesAt(fileText []string, x int, y int) int { // don' worry about it chief
-	width := len(fileText[0])
-	height := len(fileText)
-	total := 0
-	canAppearLeft := x >= 3
-	canAppearUp := y >= 3
-	canAppearRight := width-(x+1) >= 3
-	canAppearDown := height-(y+1) >= 3
-	// left case
-	if canAppearLeft &&
-		fileText[y][x-0] == 'X' &&
-		fileText[y][x-1] == 'M' &&
-		fileText[y][x-2] == 'A' &&
-		fileText[y][x-3] == 'S' {
-		total++
-	}
-	// up case
-	if canAppearUp &&
-		fileText[y-0][x] == 'X' &&
-		fileText[y-1][x] == 'M' &&
-		fileText[y-2][x] == 'A' &&
-		fileText[y-3][x] == 'S' {
-		total++
-	}
-	// down case
-	if canAppearDown &&
-		fileText[y+0][x] == 'X' &&
-		fileText[y+1][x] == 'M' &&
-		fileText[y+2][x] == 'A' &&
-		fileText[y+3][x] == 'S' {
-		total++
-	}
-	// right case
-	if canAppearRight &&
-		fileText[y][x+0] == 'X' &&
-		fileText[y][x+1] == 'M' &&
-		fileText[y][x+2] == 'A' &&
-		fileText[y][x+3] == 'S' {
-		total++
-	}
-	// upleft
-	if canAppearUp && canAppearLeft &&
-		fileText[y-0][x-0] == 'X' &&
-		fileText[y-1][x-1] == 'M' &&
-		fileText[y-2][x-2] == 'A' &&
-		fileText[y-3][x-3] == 'S' {
-		total++
-	}
-	// upright
-	if canAppearUp && canAppearRight &&
-		fileText[y-0][x+0] == 'X' &&
-		fileText[y-1][x+1] == 'M' &&
-		fileText[y-2][x+2] == 'A' &&
-		fileText[y-3][x+3] == 'S' {
-		total++
-	}
-	// downright
-	if canAppearDown && canAppearRight &&
-		fileText[y+0][x+0] == 'X' &&
-		fileText[y+1][x+1] == 'M' &&
-		fileText[y+2][x+2] == 'A' &&
-		fileText[y+3][x+3] == 'S' {
-		total++
-	}
-	// downleft
-	if canAppearDown && canAppearLeft &&
-		fileText[y+0][x-0] == 'X' &&
-		fileText[y+1][x-1] == 'M' &&
-		fileText[y+2][x-2] == 'A' &&
-		fileText[y+3][x-3] == 'S' {
-		total++
-	}
-	return total
+func getMatchesAt(fileText []string, x int, y int) bool { // don' worry about it chief
+	return (fileText[y-1][x-1] == 'M' && fileText[y+1][x+1] == 'S' ||
+		fileText[y-1][x-1] == 'S' && fileText[y+1][x+1] == 'M') &&
+		(fileText[y-1][x+1] == 'M' && fileText[y+1][x-1] == 'S' ||
+			fileText[y-1][x+1] == 'S' && fileText[y+1][x-1] == 'M')
 }
 
 func main() {
 	xmasFinds := 0
 	fileText, _ := fileToSliceOfStrings(fileName)
 	fmt.Println(fileText)
-	for y := 0; y < len(fileText); y++ {
-		for x := 0; x < len(fileText[0]); x++ {
-			if fileText[y][x] != 'X' {
+	for y := 1; y < len(fileText)-1; y++ {
+		for x := 1; x < len(fileText[0])-1; x++ {
+			if fileText[y][x] != 'A' {
 				continue
 			}
-			xmasFinds += getMatchesAt(fileText, x, y)
+			if getMatchesAt(fileText, x, y) {
+				xmasFinds++
+			}
 		}
 	}
 	fmt.Println(fileText, "\n", xmasFinds)
